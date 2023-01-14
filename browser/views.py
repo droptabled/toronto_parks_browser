1ad96946a952f5fd24181979c0c202f6cfa7c416
Mike <minghan.mike.li@gmail.com>
2023-01-14 11:09:11 -0500

    React setup for browser

--- browser/views.py
+++ browser/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "browser/index.html")
+
+def facilities(request):
+    return render(request, "browser/facilities.html")
