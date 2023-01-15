90a2b7496a3e599372847e24f3799eb3bdff1fd4
Mike <minghan.mike.li@gmail.com>
2023-01-14 20:55:32 -0500

    react backend

--- browser/urls.py
+++ browser/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Views
     path('', views.index, name='home'),
+
+    # React components
+    path('api/facilities', views.facilities, name='api-facilities')
 ]
--- browser/views.py
+++ browser/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-
-from django.template import loader
-from django.http import HttpResponse
+from django.http import JsonResponse
+from parser.models import *
 
 
 def index(request):
     return render(request, "browser/index.html")
 
 def facilities(request):
-    return render(request, "browser/facilities.html")
+    data = { "facilities": []}
+    for facility in Facility.objects.prefetch_related("tiers"):
+        facility_data = {
+            "id": facility.id,
+            "name": facility.name,
+            "link": facility.page_link(),
+            "image_link": facility.image_url,
+            "alt_text": facility.alt_text,
+            "available_tiers": []
+        }
+
+        for tier in facility.tiers.all():
+            facility_data["available_tiers"].append(tier.tier)
+
+        data["facilities"].append(facility_data)
+    return JsonResponse(data)
